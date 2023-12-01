f6dc46e8907a752601828d0931d41b805cb8a7d3
Yana Mishyna <yana.mishina.92@gmail.com>
2023-11-30 20:36:39 -0500

    trying to fix a bug on login

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -19,10 +19,12 @@ module.exports = {
   // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
   async createUser({ body }, res) {
     const user = await User.create(body);
+    console.log(user)
 
-    if (!user) {
-      return res.status(400).json({ message: 'Something is wrong!' });
-    }
+    // if (!user) {
+    //   return res.status(400).json({ message: 'Something is wrong!' });
+    // }
+
     const token = signToken(user);
     res.json({ token, user });
   },
