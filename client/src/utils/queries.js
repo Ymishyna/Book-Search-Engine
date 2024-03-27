67aee9418858ec31707394f735f0483664eee64e
Yana Mishyna <yana.mishina.92@gmail.com>
2024-03-26 21:16:19 -0400

    trying to deploy on heroku

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,23 +1,3 @@
-// import { gql } from "@apollo/client";
-
-// export const GET_ME = gql`
-//   {
-//     me {
-//       _id
-//       username
-//       email
-//       savedBooks {
-//         bookId
-//         authors
-//         image
-//         link
-//         title
-//         description
-//       }
-//     }
-//   }
-// `;
-
 import { gql } from '@apollo/client';
 
 export const QUERY_ME = gql`
--- server/schema/index.js
+++ server/schema/index.js
@@ -1,6 +1,6 @@
 // Exporting my typeDefs and resolvers.
 
-const typeDefs = require("./typeDefs");
-const resolvers = require("./resolvers");
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
 
 module.exports = { typeDefs, resolvers };
